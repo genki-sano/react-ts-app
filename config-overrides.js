492c8b3a130f98624461765507c5d2d3df2cd8a0
g-sano <g.sano.2441@gmail.com>
2020-02-23 12:37:24 +0900

    delete react-app-rewired

--- config-overrides.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = config => {
-  config.resolve = {
-    ...config.resolve,
-    alias: { '@': path.resolve(__dirname, 'src') },
-  };
-  return config;
-};
